b2e6b48f93edb4df244155c628e55817d2cb11d1
André <nopvksmu123456@gmail.com>
2025-03-21 21:01:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ titulo.innerHTML = 'Jogo de Advinhação';
 let subTitulo = document.querySelector('p');
 subTitulo.innerHTML = 'Escolha um número entre 1 e 100';
 
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10) + 1;
-}
-
 let numeroSecreto = gerarNumeroAleatorio();
 console.log(numeroSecreto);
 
@@ -15,19 +11,33 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1' , 'Jogo de Adivinhação')
-exibirTextoNaTela('p' , 'Escolha um Número entre 1 e 100')
-
-function verificarChute() {
-    let chute = parseInt(document.querySelector('input').value);
-    console.log(chute === numeroSecreto);
-}
+exibirTextoNaTela('h1', 'Jogo de Adivinhação');
+exibirTextoNaTela('p', 'Escolha um Número entre 1 e 100');
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 100) + 1;
 }
 
+let tentativas = 10;
+
 function verificarChute() {
+    console.log(tentativas)
     let chute = parseInt(document.querySelector('input').value);
     console.log(chute === numeroSecreto);
+    if (tentativas > 0) {
+        if (chute === numeroSecreto) {
+            exibirTextoNaTela('h1', 'Parabéns! Você Acertou!');
+            exibirTextoNaTela('p', `O Número Secreto é ${numeroSecreto}.`);
+        } else if (chute > numeroSecreto) {
+            exibirTextoNaTela('h1', 'Errou! O Número Secreto é Menor.');
+            exibirTextoNaTela('p', `Você possui ${tentativas} Tentativas!`);
+        } else {
+            exibirTextoNaTela('h1', 'Errou! O Número Secreto é Maior.');
+            exibirTextoNaTela('p', `Você possui ${tentativas} Tentativas!`);
+        }
+        tentativas--;
+    } else {
+        exibirTextoNaTela('h1', 'Errou!')
+        exibirTextoNaTela('P', `Você não possui mais Tentativas.`)
+    }
 }
